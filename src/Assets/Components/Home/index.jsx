5d823fa20307a3534a8d875afdaa0dcd8f51e31f
unknown <dandarabiancas@gmail.com>
2023-09-24 18:20:31 -0300

    Ajustando estilos

--- src/Assets/Components/Home/index.jsx
+++ src/Assets/Components/Home/index.jsx
@@ -5,7 +5,8 @@ export default function Home() {
   return (
     <div>
       <div className="home-container">
-        <h2>Obrigada pela compra!!</h2>
+        <h2>Obrigada pela compra!</h2>
+        <span>É um prazer ter você como cliente!</span>
       </div>
     </div>
   );
--- src/Assets/Components/Menu/index.jsx
+++ src/Assets/Components/Menu/index.jsx
@@ -4,7 +4,13 @@ import './styles.css';
 export default function Menu(){
     return(
     <div className="menu-container">
-        <h2>É um prazer ter você como cliente! Queremos agradecer pela compra e desejar que ela lhe satisfaça e supere suas expectativas.</h2>
+        <h1>Queremos agradecer pela compra e desejar que ela lhe satisfaça e supere suas expectativas.</h1>
+
+        <h2>Vá ao nosso website e deixe sua avalição</h2>
+
+        <div className="button-container">
+            <span>Clique no botão abaixo!</span>
+        </div>
     </div>
     );
 }
