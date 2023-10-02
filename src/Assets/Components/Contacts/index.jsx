768fd54c45e9d9d4ccb31c386c6db065736da2b5
unknown <dandarabiancas@gmail.com>
2023-10-02 20:38:39 -0300

    fazendo alguns ajustes

--- src/Assets/Components/Contacts/index.jsx
+++ src/Assets/Components/Contacts/index.jsx
@@ -4,7 +4,7 @@ import "./styles.css";
 export default function Contacts(){
     return(
         <div className="contact-container">
-            <h2>Alguma coisa errada? Envie-nos um e-mail para formulario@gmail.com</h2>
+            <h2>Alguma coisa errada? Envie um e-mail para formulario@gmail.com</h2>
         </div>
     );
 }
